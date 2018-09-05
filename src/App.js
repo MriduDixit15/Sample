5d030a65d1658f1be4afe124f3369a0605e8fa6b
Your Name <you@example.com>
2018-09-05 13:09:44 +0530

    meidu

---
+++ src/App.js
@@ -0,0 +1,117 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+  
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}*/
+
+
+import React, { Component } from 'react'
+
+import AuthScreen from './containers/AuthScreen'
+import HomeScreen from './containers/HomeScreen'
+
+/**
+ * The root component of the application.
+ * In this component I am handling the entire application state, but in a real app you should
+ * probably use a state management library like Redux or MobX to handle the state (if your app gets bigger).
+ */
+export class LoginAnimation extends Component {
+  state = {
+    isLoggedIn: false, // Is the user authenticated?
+    isLoading: false, // Is the user loggingIn/signinUp?
+    isAppReady: false // Has the app completed the login animation?
+  }
+
+  /**
+   * Two login function that waits 1000 ms and then authenticates the user succesfully.
+   * In your real app they should be replaced with an API call to you backend.
+   */
+  _simulateLogin = (username, password) => {
+    this.setState({ isLoading: true })
+    setTimeout(() => this.setState({ isLoggedIn: true, isLoading: false }), 1000)
+  }
+
+  _simulateSignup = (username, password, fullName) => {
+    this.setState({ isLoading: true })
+    setTimeout(() => this.setState({ isLoggedIn: true, isLoading: false }), 1000)
+  }
+
+  /**
+   * Simple routing.
+   * If the user is authenticated (isAppReady) show the HomeScreen, otherwise show the AuthScreen
+   */
+  render () {
+    if (this.state.isAppReady) {
+      return (
+        <HomeScreen
+          logout={() => this.setState({ isLoggedIn: false, isAppReady: false })}
+        />
+      )
+    } else {
+      return (
+        <AuthScreen
+          login={this._simulateLogin}
+          signup={this._simulateSignup}
+          isLoggedIn={this.state.isLoggedIn}
+          isLoading={this.state.isLoading}
+          onLoginAnimationCompleted={() => this.setState({ isAppReady: true })}
+        />
+      )
+    }
+  }
+}
+
+export default LoginAnimation
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
